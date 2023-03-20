f59a69544fc7738c77ddcc2bca9874226fd6ac3b
asiffaizan <asiffaizan0077@gmail.com>
2023-03-20 11:03:11 +0600

    deploying files

---
+++ index.js
@@ -0,0 +1,8 @@
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click",function(){
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+})

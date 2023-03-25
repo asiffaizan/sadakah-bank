51bdcfc22c420c49330405b056c831bb73e5494e
asiffaizan <asiffaizan0077@gmail.com>
2023-03-25 10:47:51 +0600

    adding functionality on deposit section

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//login button event handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click",function(){
     const loginArea = document.getElementById("login-area");
@@ -5,4 +6,20 @@ loginBtn.addEventListener("click",function(){
     
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
+})
+
+//deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber= parseFloat(depositAmount)
+    // console.log(depositNumber);
+
+    const currentDeposit = document.getElementById("currentDeposit").innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+    
+    document.getElementById("currentDeposit").innerText = totalDeposit;
+    document.getElementById("depositAmount").value = "";
+
 })
